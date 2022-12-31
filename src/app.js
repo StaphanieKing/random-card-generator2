1aa8c7d3f4cb5499ebaf35d25cc1d0e52ca9ed44
StaphanieKing <staphaniek@yahoo.com>
2022-12-31 01:59:38 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
 };
+const suits = ["♠", "♣", "♥", "♦"];
+
+let newNumber = document.querySelector(".number");
+let topSuit = document.querySelector(".top-suit");
+//create new variable for bottom-suit
+let bottomSuit = document.querySelector(".bottom-suit");
+
+function randomCard(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
+function iconCard(suits) {
+  return Math.floor(Math.random() * suits.length);
+}
+function card() {
+  let number = number[randomCard(number)];
+
+  let icons = suits[iconCard(suits)];
+
+  if (icons == suits[2] || icons == suits[3]) {
+    document.getElementsByClassName("top-suit")[0].style.color = "red";
+    document.getElementsByClassName("bottom-suit")[0].style.color = "red";
+  } //create variable that uses the random function to get a new suit (similar to line 15)
+
+  newNumber.innerHTML = number;
+  topSuit.innerHTML = icons;
+  bottomSuit.innerHTML = icons;
+
+  //assign the new suit to the top and bottom variables using innerHTML (lines 12 and 13)
+}
